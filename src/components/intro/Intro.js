01e249bbf6fee3e21449dbdf5eeace5b1dc82444
lukaswarren <lukaswarren728@gmail.com>
2024-01-03 10:19:10 -0700

    Some changes to css

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -4,13 +4,18 @@ import profile from '../../assets/profilePic.png'
 import {Link} from 'react-scroll'
 
 export const Intro = () => {
+ 
+
   return (
     <section id="intro">
         <div className="introContent">
             <span className='hello'>Hello</span><br/>
             <span className='introText'> I'm<span className="introName"> Luke Warren </span><br/> <span className='title'>A Backend Software Engineer</span></span>
             <p className="introPara">I am software engineer with 7+ years experience in Backend Development</p>
-            <Link><button className='hireBtn'>Hire Me</button></Link>
+            <div className='introBtns'>
+              <Link><button className='hireBtn' >Hire Me</button></Link>
+              <a href='/LukasWarrenResume.pdf' download><button className='hireBtn' >Download My Resume</button></a>
+            </div>
         </div>
         <img src={profile} alt='Profile' className="profilePic"/>
     </section>
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -4,42 +4,47 @@ import './Projects.css'
 const Projects = () => {
   return (
     <section id='projects'>
-        <div className='project'>
-        <iframe src="https://giphy.com/embed/bY15rKzgQ2EUvGfSKw"  class="giphy-embed" allowFullScreen></iframe>
-            <div className='projectData'>
-                <span className='title'>Info</span>
-                <span className='project link'> GitHub Link </span>
+        <span className='projectTitle'>Projects</span>
+        <span className='projectDesc'>Here's a few of my personal projects. You can check out the code using the Github link.  You can also run them on a docker container. Feel free to contact me to ask about any of these.</span>
+        <div className='rows'>
+            <div className='project'>
+            <iframe src="https://giphy.com/embed/bY15rKzgQ2EUvGfSKw"  class="giphy-embed" className='animation' allowFullScreen></iframe>
+                <div className='projectData'>
+                    <span className='title'>Info</span>
+                    <span className='project link'> GitHub Link </span>
+                </div>
+            </div>
+            <div className='project'>
+                <img className='animation'></img>
+                <div className='projectData'>
+                    <span className='title'>Info</span>
+                    <span className='project link'> GitHub Link </span>
+                </div>
+            </div>
+            <div className='project'>
+                <img className='animation'></img>
+                <div className='projectData'>
+                    <span className='title'>Info</span>
+                    <span className='project link'> GitHub Link </span>
+                </div>
             </div>
         </div>
-        <div className='project'>
-            <img className='animation'></img>
-            <div className='projectData'>
-                <span className='title'>Info</span>
-                <span className='project link'> GitHub Link </span>
+        <div className='rows'>
+            <div className='project'>
+                <img className='animation'></img>
+                <div className='projectData'>
+                    <span className='title'>Info</span>
+                    <span className='project link'> GitHub Link </span>
+                </div>
+            </div>
+            <div className='project'>
+                <img className='animation'></img>
+                <div className='projectData'>
+                    <span className='title'>Info</span>
+                    <span className='project link'> GitHub Link </span>
+                </div>
             </div>
         </div>
-        <div className='project'>
-            <img className='animation'></img>
-            <div className='projectData'>
-                <span className='title'>Info</span>
-                <span className='project link'> GitHub Link </span>
-            </div>
-        </div>
-        <div className='project'>
-            <img className='animation'></img>
-            <div className='projectData'>
-                <span className='title'>Info</span>
-                <span className='project link'> GitHub Link </span>
-            </div>
-        </div>
-        <div className='project'>
-            <img className='animation'></img>
-            <div className='projectData'>
-                <span className='title'>Info</span>
-                <span className='project link'> GitHub Link </span>
-            </div>
-        </div>
-
     </section>
   )
 }
