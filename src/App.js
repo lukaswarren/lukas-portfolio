81fa25d1cce8a8a0bce3be0459b9c017ad9b434e
lukaswarren <lukaswarren728@gmail.com>
2024-01-09 10:16:09 -0700

    Further about

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Intro from "./components/intro/Intro";
 import Projects from "./components/projects/Projects"
 import Skills from "./components/skills/Skills";
 import About from "./components/about/About"
+import Social from "./components/social/Social";
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
       <Projects/>
       <Skills/>
       <About/>
+      <Social/>
     </div>
   );
 }
--- src/components/about/About.js
+++ src/components/about/About.js
@@ -11,7 +11,7 @@ const About = () => {
             <div className="aboutContent">
                 <p className="aboutPara"> I got my Bachelors degree in Mathematics while minoring in Computer Science at Boise State. After graduating I went on to work at a company called Clearwater Analytics.</p><br/>
                 <p className="aboutPara">There I helped build web-services in Java using SpringBoot and Maven. Working in data processing, building and maintaining ETLs that processed investment accounting data. </p><br/>
-                <p className="aboutPara">After 4 and half years at Clearwater I took some time off before starting as an Instructor and SME Engineer at Kenzie Academy. There I've been helping teach students how to build web-services that run on AWS using Lambda and DynamoDB.</p>
+                <p className="aboutPara">After 4 and half years at Clearwater I took some time off before starting as an Instructor and SME Engineer at Kenzie Academy. There I've been helping teach students how to build web-services that run on AWS using Lambda and DynamoDB. I helped automate the testing of the assignments building out unit tests and Gradle tasks in Github actions and workflows.</p>
             </div>
         </div>
     
